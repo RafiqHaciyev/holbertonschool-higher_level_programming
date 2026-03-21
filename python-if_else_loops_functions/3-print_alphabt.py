8b5ec0b8cb01fc074824bd85149101a9c7f348a6
Rafig <13640@holbertonstudents.com>
2026-03-21 19:46:40 +0000

    Print alphabet but not qe

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,2 +1,4 @@
 #!/usr/bin/python3
-print("{:s}".format("".join(chr(97 + i) for i in range(26) if chr(97 + i) not in "qe")), end="")
+print("{:s}".format(
+    "".join(chr(97 + i) for i in range(26) if chr(97 + i) not in "qe")),
+      end="")
