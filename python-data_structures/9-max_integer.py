890d6ba066226c4d0009eeaf64f4c3f8631e4fb1
Rafig <13640@holbertonstudents.com>
2026-03-28 18:24:10 +0000

    Max integer

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
-def multiple_returns(sentence):
-    if len(sentence) == 0:
-        return (0, None)
-    return (len(sentence), sentence[0])
+def max_integer(my_list=[]):
+    # Check if the list is empty
+    if not my_list:
+        return None
+
+    # Initialize the biggest number with the first element
+    biggest = my_list[0]
+
+    # Iterate through the list starting from the second element
+    for num in my_list:
+        if num > biggest:
+            biggest = num
+
+return biggest
