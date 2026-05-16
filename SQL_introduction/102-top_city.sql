85ab2ed35d0f954a6beb56a1ae05c1b4264305f0
Rafig <rafiqhaciyev2006@gmail.com>
2026-05-16 14:21:58 +0000

    Top city

---
+++ SQL_introduction/102-top_city.sql
@@ -0,0 +1,2 @@
+-- Display top 3 cities temperature in July and August ordered by temp descending
+SELECT city, AVG(value) AS avg_temp FROM temperatures WHERE month IN (7, 8) GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
